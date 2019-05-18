4141098d4c67e812dbdd25c26d3bcaf3013e5476
td94 <quynhtrangdo94@gmail.com>
2019-05-17 20:33:41 -0400

    Create App component

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export default function App(): JSX.Element {
+  return (
+    <React.Fragment>
+      <h1>Rick and Morty</h1>
+      <p>Pick your favorite episode!!</p>
+    </React.Fragment>
+  )
+}
