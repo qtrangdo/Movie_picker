f067d738f7c5727b61fa4f271602a06e5362aab4
td94 <quynhtrangdo94@gmail.com>
2019-05-19 00:12:47 -0400

    Fetcf data action

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,23 @@ import * as React from 'react';
 import { Store } from './Store';
 
 export default function App(): JSX.Element {
-  const store = React.useContext(Store);
+  const {state, dispatch} = React.useContext(Store);
+
+  React.useEffect(() => {
+    state.episodes.length === 0 && fetchDataAction()
+  })
+
+  const fetchDataAction = async () => {
+    const data = await fetch('https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes');
+    const dataJSON = await data.json();
+    return dispatch({
+      type: 'FETCH_DATA',
+      payload: dataJSON._embedded.episodes
+    })
+  }
+  
   return (
     <React.Fragment>
-      {console.log(store)}
       <h1>Rick and Morty</h1>
       <p>Pick your favorite episode!!</p>
     </React.Fragment>
