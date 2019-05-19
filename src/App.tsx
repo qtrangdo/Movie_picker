886ba92fdc43b3ae1574c2b5229dba012f2b6558
td94 <quynhtrangdo94@gmail.com>
2019-05-19 00:33:20 -0400

    Render episodes thumnails

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { Store } from './Store';
 
+
+
 export default function App(): JSX.Element {
   const {state, dispatch} = React.useContext(Store);
 
@@ -16,11 +18,24 @@ export default function App(): JSX.Element {
       payload: dataJSON._embedded.episodes
     })
   }
-  
+  console.log(state)
   return (
     <React.Fragment>
       <h1>Rick and Morty</h1>
       <p>Pick your favorite episode!!</p>
+      <section>
+        {state.episodes.map((episode: any) => {
+          return (
+            <section key = {episode.id}>
+              <img src={episode.image ? episode.image.medium : episode.image } alt={`Rick & Morty ${episode.name}`}/>
+              <div>{episode.name}</div>
+              <section>
+                Season: {episode.season} Number: {episode.number}
+              </section>
+            </section>
+          )
+        })}
+      </section>
     </React.Fragment>
   )
 }
