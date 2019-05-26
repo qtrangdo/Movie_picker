10e4eedb8e56e420cd646f1deb4177369aad59aa
td94 <quynhtrangdo94@gmail.com>
2019-05-27 01:24:08 +0200

    Populate Faves page

--- src/FavPage.tsx
+++ src/FavPage.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Store } from './Store';
+import { IEpisodeProps } from './interfaces';
+import { toggleFavAction } from './Actions';
 
-export default function FavPage() {
+const EpisodeList = React.lazy<any>(() => import('./EpisodeList'))
+
+export default function FavPage(): JSX.Element {
+  const { state, dispatch } = React.useContext(Store);
+
+  const props: IEpisodeProps = {
+    episodes: state.favorites,
+    store: {state, dispatch},
+    toggleFavAction,
+    favorites: state.favorites,
+  }
+
   return (
-    <div>
-      test
-    </div>
+    <React.Fragment>
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <section className="episode-layout">
+          <EpisodeList {...props} />
+        </section>
+      </React.Suspense>
+    </React.Fragment>
   )
 }
